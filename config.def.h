e47e536154d1c16ccbf454e6c41de0f52b41179c
Fikri Rahmat Nurhidayat <fikrirnurhidayat@gmail.com>
2020-11-30 21:57:51 +0700

    Ignore build files

--- config.def.h
+++ config.def.h
@@ -62,6 +62,8 @@ static const char unknown_str[] = "n/a";
  * wifi_essid          WiFi ESSID                      interface name (wlan0)
  */
 static const struct arg args[] = {
-	/* function format          argument */
-	{ datetime, "%s",           "%F %T" },
+	{ battery_state, " %s ",              "BAT0" },
+	{ battery_perc,  "   %s ",           "BAT0" },
+	{ battery_perc,  "   %s ",           "BAT1" },
+	{ datetime,      " %s ",               "%F %T" },
 };
